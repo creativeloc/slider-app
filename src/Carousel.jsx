bcbbcd424359f5662646255f7cd1815d363d2a0a
Lawrence Caulker <locaulker@gmail.com>
2025-05-24 01:15:20 -0400

    add slider functionality

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react"
 import { shortList, list, longList } from "./data"
 import { FaQuoteRight } from "react-icons/fa"
-import { FiChevronLeft, FiChevronRight, FiChevronsLeft } from "react-icons/fi"
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
 
 const Carousel = () => {
-  const [people, setPeople] = useState(list)
+  const [people, setPeople] = useState(longList)
+  const [currentPerson, setCurrentPerson] = useState(0)
 
-  const prevSlide = () => {}
-  const nextSlide = () => {}
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length
+      return result
+    })
+  }
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length
+      return result
+    })
+  }
 
   return (
     <section className="slider-container">
@@ -16,14 +27,18 @@ const Carousel = () => {
         return (
           <article
             className="slide"
-            style={{ transform: `translateX(${100 * personIndex}%)` }}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? "visible" : "hidden"
+            }}
             key={id}
           >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
             <p className="text">{quote}</p>
-            <FaQuoteRight />
+            <FaQuoteRight className="icon" />
           </article>
         )
       })}
